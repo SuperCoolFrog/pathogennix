7aca79939a2c38d8af2855172a54793968cd1840
Michael Vasquez <michael.vasquez@vertexinc.com>
2020-04-27 15:34:20 -0400

    Clean up some styles

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from './logo.module.scss';
+import history from '../../routes/history';
 
 const Logo = () => {
   
-  return (<div className={styles.container}>
+  const handleClick = () => {
+    history.push('/');
+    return false;
+  };
+  
+  return (<div className={styles.container} onClick={handleClick}>
     <span className={styles.pathogen}>Pathogen</span><span className={styles.nix}>Nix</span>
   </div>);
 };
