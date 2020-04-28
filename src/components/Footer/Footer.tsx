7f45968bb9452e4faa1f18e03b25bd9d51b19637
Michael Vasquez <michael.vasquez@vertexinc.com>
2020-04-27 22:20:07 -0400

    Back to Top Button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import styles from './footer.module.scss';
 
 const Footer = () => {
   return (<footer className={styles.footerContainer}>
+    <button className={styles.backToTopButton}>Back to Top</button>
     Hello World
   </footer>);
 };
