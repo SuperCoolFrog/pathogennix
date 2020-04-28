9b279db58be198041cb7d0df5e72098f9fd175a8
Michael Vasquez <michael.vasquez@vertexinc.com>
2020-04-28 00:08:12 -0400

    Remove code for testing Drawer

--- src/components/ModalCollection/ModalCollection.tsx
+++ src/components/ModalCollection/ModalCollection.tsx
@@ -1,21 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Drawer from '../Drawer/Drawer';
+import React from 'react';
 // import { useSelector } from 'react-redux';
 // import NewInventoryModal from '../../features/NewInventoryModal/NewInventoryModal';
 // import { newInventoryModalIsVisibleSelector } from '../../store/inventory/inventory-selector';
 
 const ModalCollection = () => {
   // const newInventoryModalIsVisible = useSelector(newInventoryModalIsVisibleSelector);
-  const [isOpen, setOpen] = useState(false);
-  
-  useEffect(() => {
-    setTimeout(() => { setOpen(true); }, 1000);
-  }, [])
-  
-
   return (<>
     {/* newInventoryModalIsVisible && <NewInventoryModal /> */}
-    <Drawer onClose={() => {}} renderBody={() => <div>Hello</div>} open={isOpen} left />
   </>);
 };
 
