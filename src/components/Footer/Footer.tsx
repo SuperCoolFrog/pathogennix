21dcc1468580714a940dfc6c2936131549f49fe3
Michael Vasquez <michael.vasquez@vertexinc.com>
2020-04-27 22:12:32 -0400

    Fix layout scroll

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './footer.module.scss';
+
+const Footer = () => {
+  return (<footer className={styles.footerContainer}>
+    Hello World
+  </footer>);
+};
+
+export default Footer;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { inventoryStateSelector } from '../../store/inventory/inventory-selector';
 import { getItems } from '../../store/inventory/inventory-thunks';
 import ItemList from '../../components/ItemList/ItemList';
-
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -35,6 +35,9 @@ const Home = () => {
       <div className={styles.itemListContainer}>
         <ItemList header="Sanitizer" items={inventoryItems} />
       </div>
+    </div>
+    <div className={"pure-u-1"}>
+      <Footer />
     </div>
   </section>);
 };
