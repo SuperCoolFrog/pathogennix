e318ec255e67024c9b96be97aa9db905e0eece55
Michael Vasquez <michael.vasquez@vertexinc.com>
2020-04-27 22:34:40 -0400

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './footer.module.scss';
 
 const Footer = () => {
   return (<footer className={styles.footerContainer}>
     <button className={styles.backToTopButton}>Back to Top</button>
-    Hello World
+    <div className={classNames("pure-g", styles.footerContent)}>
+      <div className={classNames("pure-u-1-3", styles.footerContentColumn)}>
+        <header>See our Products</header>
+        <ul>
+          <li>Home</li>
+          <li>Masks</li>
+          <li>Sanitizer</li>
+        </ul>
+      </div>
+      <div className={classNames("pure-u-1-3", styles.footerContentColumn)}>
+        <header>About Us</header>
+        <ul>
+          <li>About</li>
+          <li>South Mountain Tech llc</li>
+        </ul>
+      </div>
+    </div>
   </footer>);
 };
 
