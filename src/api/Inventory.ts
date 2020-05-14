198ae8df15f8eff0e1ac691a2a21279be02c7115
Michael Vasquez <michael.vasquez@vertexinc.com>
2020-05-14 16:50:13 -0400

    Working fetchItemsCall

--- src/api/Inventory.ts
+++ src/api/Inventory.ts
@@ -8,127 +8,40 @@ import InventoryItem from '../models/InventoryItem';
 // 
 // type SubscriptionId = string;
 // 
-interface ItemsAPI {
-  getInventory: () => Promise<InventoryItem[]>;
+// interface ItemsAPI {
+//  getInventory: () => Promise<InventoryItem[]>;
 //   subscribeToInventory: (itemsUpdated: itemsUpdatedHandler) => Promise<SubscriptionId>;
 //   unsubscribeFromInventory: (subscriptionId: string) => void;
 //   postNewInventory: (userId: string, form: HashMap<FormField, string | File>, imageUrl: string) => Promise<void>;
-}
+// }
 
 // type itemsUpdatedHandler = (items: Item[]) => void;
 // 
 // const SUBSCRIPTION_CACHE: Map<string, () => void> = new Map();
 // 
-const itemsAPI: ItemsAPI = {
-  getInventory: async () => {
-    const mockData: InventoryItem[] = [
-      { itemId: "1", active: true, category: "1", created: new Date(), description: "Something Something",
-        imageSrc: "/mask.placeholder.png", itemName: "Something 1", price: 1.11, quantity: 11, sku: "Blue",
-      },
-      { itemId: "2", active: true, category: "2", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 2", price: 2.22, quantity: 22, sku: "Blue",
-      },
-      { itemId: "3", active: true, category: "3", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 3", price: 3.33, quantity: 33, sku: "Blue",
-      },
-      { itemId: "4", active: true, category: "4", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 4", price: 4.44, quantity: 44, sku: "Blue",
-      },
-      { itemId: "5", active: true, category: "5", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 5", price: 5.55, quantity: 55, sku: "Blue",
-      },
-      { itemId: "6", active: true, category: "6", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 6", price: 6.66, quantity: 66, sku: "Blue",
-      },
-      { itemId: "7", active: true, category: "7", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 7", price: 7.77, quantity: 77, sku: "Blue",
-      },
-      { itemId: "8", active: true, category: "8", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 8", price: 8.88, quantity: 88, sku: "Blue",
-      },
-      { itemId: "9", active: true, category: "9", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 9", price: 9.99, quantity: 99, sku: "Blue",
-      },
-      { itemId: "10", active: true, category: "10", created: new Date(), description: "Something Something",
-        imageSrc: "", itemName: "Something 10", price: 10.1010, quantity: 1010, sku: "Blue",
-      },
-    ];
-    
-    return Promise.resolve(mockData);
-  }, 
+// const itemsAPI: ItemsAPI = {
 //   getInventory: async () => {
-//     const db = firebase.firestore();
-//     const itemsCollection = db.collection('items');
-//     const query = itemsCollection.where('active', '==', true);
-//     let storeItems: Item[] = [];
-// 
-//     const snapshot = await query.get();
-// 
-//     snapshot.forEach((doc) => {
-//       const item: Item = {
-//         itemId: doc.id as string,
-//         ...doc.data() as any,
-//       };
-//       storeItems = [...storeItems, item];
-//     });
-// 
-//     return storeItems;
-//   },
-// 
-//   subscribeToInventory: async (itemsUpdated: itemsUpdatedHandler) => {
-//     const db = firebase.firestore();
-//     const itemsCollection = db.collection('items');
-//     const query = itemsCollection.where('active', '==', true);
-// 
-//     const unsubscribe = query.onSnapshot((snapshot) => {
-//       let storeItems: Item[] = [];
-//       snapshot.forEach((doc) => {
-//         const item: Item = {
-//           itemId: doc.id as string,
-//           ...doc.data() as any,
-//         };
-//         storeItems = [...storeItems, item];
-//       });
-//       itemsUpdated(storeItems);
-//     });
-// 
-//     const subscriptionUUID = createUUID();
-// 
-//     SUBSCRIPTION_CACHE.set(subscriptionUUID, unsubscribe);
-// 
-//     return subscriptionUUID;
-//   },
-// 
-//   unsubscribeFromInventory: (subscriptionId: string) => {
-//     if (SUBSCRIPTION_CACHE.has(subscriptionId)) {
-//       const unsubscribe = SUBSCRIPTION_CACHE.get(subscriptionId);
-//       unsubscribe && unsubscribe();
-//       SUBSCRIPTION_CACHE.delete(subscriptionId);
-//     }
-//   },
-//   
-//   postNewInventory: async (userId: string, form: HashMap<FormField, string | File>, imageUrl: string) => {
-//     const itemName = form.get(FormField.itemName) as string;
-//     const quantity = form.get(FormField.quantity) as string;
-//     const price = form.get(FormField.price) as string;
-//     const description = form.get(FormField.description) as string;
-//     // @TODO: add sku
-//     // @TODO: add active checkbox to form
-//     const userCollection = firebase.firestore().collection('users');
-//     const currentUserDoc = userCollection.doc(userId);
-//     const inventoryCollection = currentUserDoc.collection(inventoryPath);
-//     
-//     return inventoryCollection.add({
-//       itemName,
-//       quantity,
-//       price,
-//       description,
-//       imageSrc: imageUrl,
-//       created: (new Date()).getTime(),
-//       active: true,
-//       deleted: false,
-//     }).then();
-//   },
-};
+//     return Promise.resolve(mockData);
+//   }, 
+// };
 
-export default itemsAPI;
+class ItemsAPI {
+  
+  endpoint: string;
+  externalAppKey: string;
+  
+  constructor(endpoint: string, externalAppKey: string) {
+    this.endpoint = endpoint;
+    this.externalAppKey = externalAppKey;
+  }
+  
+  public async getInventory(): Promise<InventoryItem[]> {
+    const url = `${this.endpoint}/fetchInventoryItems`;
+    const body = JSON.stringify({externalAppKey: this.externalAppKey});
+    const response = await fetch(url, { method: 'POST', body });
+    console.log('RESPONSE', await response.json());
+    return [];
+  }
+}
+
+export default ItemsAPI;
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,29 +1,12 @@
-// import * as firebase from 'firebase';
-// import paymentsAPI from './Payments';
 import configAPI from './Config';
-import inventoryAPI from './Inventory';
-// import authAPI from './Auth';
-// import storageAPI from './Storage';
+import InventoryAPI from './Inventory';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB1TJHr9FUroA1825vm6Tp9_bskfEOOs6I",
-//   authDomain: "pinnacle-1abfd.firebaseapp.com",
-//   databaseURL: "https://pinnacle-1abfd.firebaseio.com",
-//   projectId: "pinnacle-1abfd",
-//   storageBucket: "pinnacle-1abfd.appspot.com",
-//   messagingSenderId: "486091100722",
-//   appId: "1:486091100722:web:7107dbb4aba3cb9bbc622b",
-//   measurementId: "G-DXT9TDWBC5"
-// } as const;
-// 
-// const app = firebase.initializeApp(firebaseConfig);
+const apiEndpoint = 'https://us-central1-pinnacle-1abfd.cloudfunctions.net';
+const externalAppKey = '12345ABC';
 
 const api = {
-//   auth: authAPI,
   configs: configAPI,
-  inventory: inventoryAPI,
-//   payments:  paymentsAPI,
-//   storage: storageAPI,
+  inventory: new InventoryAPI(apiEndpoint, externalAppKey),
 };
 
 export default api;
