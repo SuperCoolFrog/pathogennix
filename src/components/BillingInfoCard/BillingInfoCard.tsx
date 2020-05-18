14110a0c1d19cb53c886b02a9242b25273ec00d8
Michael Vasquez <michael.vasquez@vertexinc.com>
2020-05-18 17:03:24 -0400

    Added actions to billing info card

--- src/components/BillingInfoCard/BillingInfoCard.tsx
+++ src/components/BillingInfoCard/BillingInfoCard.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 import styles from './billing-info-card.module.scss';
 import PaymentForm from '../../components/PaymentForm/PaymentForm';
+import { shoppingCartItemsSelector } from '../../store/shopping-cart/shopping-cart-selector';
+import { asPriceString } from '../../helpers/helpers';
+
 
 const ReviewCart = () => {
+  const items = useSelector(shoppingCartItemsSelector);
+
+  let subtotal = 0;
+  let processingFee = 0;
+  let shippingCost = 0;
+  
+  if (items) {
+    subtotal = items.reduce((t, cartItem) => {
+      return t + cartItem.inventoryItem.price * cartItem.quantityToBuy;
+    }, 0);
+  }
+
   return (<section className={classNames("pure-u-1", styles.cardContainer)}>
     <section>
       <header className={styles.header}>
@@ -14,6 +30,33 @@ const ReviewCart = () => {
       <div className={"pure-u-2-3"}>
         <div className={styles.contentContainer}>
           <PaymentForm />
+        </div>
+      </div>
+      <div className={"pure-u-1-3"}>
+        <div className={styles.actionsContainer}>
+          <table className={styles.totalsTable}>
+            <tbody>
+              <tr>
+                <td>Subtotal</td>
+                <td>${asPriceString(subtotal)}</td>
+              </tr>
+              <tr>
+                <td>Processing Fee</td>
+                <td>${asPriceString(processingFee)}</td>
+              </tr>
+              <tr>
+                <td>Shipping</td>
+                <td>${asPriceString(shippingCost)}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className={styles.totalContainer}>
+            <span className={styles.totalLabel}>Total:</span>
+            <span className={styles.total}>${asPriceString(subtotal + processingFee + shippingCost)}</span>
+          </div>
+          <div className={styles.placeOrderButtonContainer}>
+            <button className={styles.placeOrderButton}>Place Order</button>
+          </div>
         </div>
       </div>
     </div>
